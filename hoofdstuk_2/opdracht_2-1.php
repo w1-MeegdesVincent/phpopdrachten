e032dbc535ad4b74abf4def542d61226d7f7896e
Vincent Meegdes <vincent.meegdes@edu-kw1c.nl>
2020-02-10 20:34:13 +0100

    add opdracht_2-2.php + updated menu's

--- hoofdstuk_2/opdracht_2-1.php
+++ hoofdstuk_2/opdracht_2-1.php
@@ -32,10 +32,10 @@
         <li>Hoofdstuk 2
             <ul>
                 <li>
-                    <a href="hoofdstuk2/opdracht_2-1.php">Opdracht 2.1</a>
+                    <a href="opdracht_2-1.php">Opdracht 2.1</a>
                 </li>
                 <li>
-                    <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 2.2</a>
+                    <a href="opdracht_2-2.php">Opdracht 2.2</a>
                 </li>
             </ul>
         </li>
---
+++ hoofdstuk_2/opdracht_2-2.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * User: Vincent Meegdes
+ * Date: 5-2-2020-
+ * Time: 19:43 PM
+ * File: opdracht_2-2.php
+ */
+?>
+<?php
+//aanmaken variabelen
+$text1 = "Hallo";
+$text2 = "een makkelijke taal";
+$text3 = "toch zo'n makkelijke taal";
+$text4 = "wat is";
+$text5 = "PHP";
+$text6 = "Nooit gedacht dat";
+$text7 = "De installatie is best ingewikkeld";
+$text8 = "Fijn toch";
+$text9 = "?";
+$text10 = ".";
+$text11 = ",";
+$text12 = "<br>";
+$text13 = "is";
+$text14 = "vind je niet";
+?>
+<!doctype html>
+<html>
+<head>
+    <title>
+        2.2.php
+    </title>
+    <!--	beschrijfing van de website-->
+    <meta name="description" content="opdracht 2.2 php">
+    <!--	steekwoorden waar de browser naar zoekt -->
+    <meta name="keywords" content="opdracht,2.2,php">
+    <!--	de type tekens die gebruikt worden -->
+    <meta charset="utf-8">
+    <!--    het linken van een css bestand -->
+    <link href="../css/style.css" type="text/css" rel=stylesheet >
+</head>
+<body>
+<header>
+    <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+</header>
+<aside>
+    <h2>Menu</h2>
+    <ul>
+        <li>Hoofdstuk 2
+            <ul>
+                <li>
+                    <a href="opdracht_2-1.php">Opdracht 2.1</a>
+                </li>
+                <li>
+                    <a href="opdracht_2-2.php">Opdracht 2.2</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</aside>
+<main id="wrapper">
+    <h2>Uitwerkingen</h2>
+    <p>
+        <!--    tekst 1    -->
+        <?php
+            echo "$text1 $text4 $text5 $text3$text10 $text12 $text7 $text10 $text8$text9 $text12 $text6 $text5 $text3 $text13$text10";
+        ?>
+    </p>
+    <p>
+        <!--    tekst 2    -->
+        <?php
+        echo "$text1$text11 $text12 $text8 $text5 $text3$text10 $text12 $text7$text10 $text14$text9";
+        ?>
+    </p>
+</main>
+</body>
+</html>
+
