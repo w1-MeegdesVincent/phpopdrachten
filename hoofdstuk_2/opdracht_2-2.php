d71e6f960a67b0013d5cdbb63211709c5ed2f683
Vincent Meegdes <vincent.meegdes@edu-kw1c.nl>
2020-02-17 10:54:23 +0100

    added changes

--- hoofdstuk_2/opdracht_2-2.php
+++ hoofdstuk_2/opdracht_2-2.php
@@ -60,16 +60,15 @@ $text14 = "vind je niet";
 </aside>
 <main id="wrapper">
     <h2>Uitwerkingen</h2>
-    <p>
         <!--    tekst 1    -->
         <?php
-            echo "$text1 $text4 $text5 $text3$text10 $text12 $text7 $text10 $text8$text9 $text12 $text6 $text5 $text3 $text13$text10";
+            echo "<p>".$text1 . $text4 . $text5 . $text3.$text10 . "</p>" . "<p>". $text12.$text7.$text10."</p>"."<p>". $text8.$text9."</p>"."<p>". $text12. $text6. $text5. $text3. $text13.$text10."</p>";
         ?>
-    </p>
+
     <p>
         <!--    tekst 2    -->
         <?php
-        echo "$text1$text11 $text12 $text8 $text5 $text3$text10 $text12 $text7$text10 $text14$text9";
+        echo "<p>"."$text1$text11"."</p>"."<p>"." $text12 $text8 $text5 $text3$text10"."</p>"."<p>"." $text12 $text7$text10"."</p>"."<p>"." $text14$text9"."</p>";
         ?>
     </p>
 </main>
