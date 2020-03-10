d3ece6e8b6bbca39ab806816eef71eb14563ae6c
Vincent Meegdes <vincent.meegdes@edu-kw1c.nl>
2020-03-10 12:13:44 +0100

    added opdracht_4-1.php + added footer to every opdracht

---
+++ hoofdstuk_4/opdracht_4-1.php
@@ -0,0 +1,17 @@
+<?php
+include "../includes/header.php";
+include "../includes/menu.php";
+?>
+
+<main id="wrapper">
+    <h2>Uitwerkingen</h2>
+    <?php
+    echo "begroeting op footer";
+    ?>
+
+</main>
+<?php
+include "../includes/footer.php";
+?>
+</body>
+</html>
