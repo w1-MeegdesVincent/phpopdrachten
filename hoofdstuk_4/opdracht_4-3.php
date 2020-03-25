6a43191ba4faf088fa69138ff6779b8fcfaead51
Vincent Meegdes <vincent.meegdes@edu-kw1c.nl>
2020-03-25 15:02:53 +0100

    added opdracht_4-3.php and script4.3.php

---
+++ hoofdstuk_4/opdracht_4-3.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * User: Vincent Meegdes
+ * Date: 25-3-2020
+ * Time: 14:28 PM
+ * File: opdracht_4.3.php
+ */
+include "../includes/header.php";//../ gebruiken voor includes
+include "../includes/menu.php";//../ gebruiken voor includes
+?>
+
+    <main id="wrapper">
+        <h2>Uitwerkingen</h2>
+        <?php
+        include "../includes/script4.3.php";
+        echo $task2;
+        echo "<br>";
+        echo $task3;
+        ?>
+    </main>
+    <?php
+    include "../includes/footer.php";//../ gebruiken voor includes
+    ?>
+    </body>
+</html>
+
---
+++ includes/script4.3.php
@@ -0,0 +1,16 @@
+<?php
+$task2 ="Lancering in:";
+
+$counter = 20;
+while($counter > 0 )
+{
+    $counter--;
+    $task2 = $task2 . " " . $counter . ",";
+}
+$task3 = "Lancering in:";
+
+for($counter2 = 20; $counter2 > 0; $counter2--)
+{
+    $task3 = $task3 . " " . $counter2 . ",";
+}
+?>
